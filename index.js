5e39432331eda11403a5a01d4992e67b17e553a3
Android-Nougat <42477290+Adhishreya@users.noreply.github.com>
2021-09-18 12:02:13 +0530

    working version v1

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var resultElement = document.getElementById("result");
 var inputValue = "";
 var shortInput = "";
 var resultArray = [];
+var menuElement = document.getElementsByClassName('hamburger')[0];
+var localStorageArray = [];
+localStorage.setItem('urlShortner',JSON.stringify(localStorageArray));
 var url = "https://api.shrtco.de/v2/shorten?url="
 urlInput.addEventListener('input',(e)=>{
     inputValue = e.target.value;
@@ -11,23 +14,49 @@ urlInput.addEventListener('input',(e)=>{
 buttonSearch.addEventListener('click',async ()=>{
     if(inputValue.length>0)
     {
-        // console.log(inputValue);
       var result = await  fetch(url+inputValue).then(res=>res.json()).then(res=>{
-            // console.log(res.result)
-            //  resultArray = 
             return  Object.values(res.result);
-            // resultArray = resultArray.slice(1);
-            // resultArray = resultArray.splice(-1);
-            // resultArray.pop();
-            // console.log(resultArray)
         });
-        // console.log(result);
         result = result.slice(1);
         result.pop();
         resultArray = result;
     }
+    var pattern = /http/;
+    var tempArray = resultArray.filter(ele=>{
+        return (!ele.match(pattern));
+    });
+    resultArray = tempArray;
+    resultArray.sort((a,b)=>a.length-b.length);
+    console.log(resultArray);
+    
+    localStorageArray = JSON.parse(localStorage.getItem('urlShortner'));
+    // if(localStorage)
+    localStorageArray.push(resultArray[1]);
 
-    console.log(resultArray)
+    Array.from(resultArray,(x)=>{
+        var newElement = document.createElement("div");
+        var newElementButton = document.createElement("button");
+        var newElementHeader = document.createElement("h5");
+        var newElementSpan = document.createElement("h4");
+        newElementHeader.innerHTML = resultArray[1];
+        newElementButton.innerHTML = "Copy";
+        newElementSpan.innerHTML = inputValue;
+        newElement.className = "result-card";
+        // newElement.appendChild(document.createElement("h1"));
+        newElementButton.className = "result-button"
+        // console.log(newElement);
+        // newElement.innerHTML = x;
+        newElement.appendChild(newElementSpan);
+        newElement.appendChild(newElementHeader);
+        newElement.appendChild(newElementButton);
+        resultElement.appendChild(newElement);
+
+        newElementButton.addEventListener('click',()=>
+        {
+            newElementButton.innerHTML = "Copied";
+            newElementButton.style.background = "hsl(257, 27%, 26%)"
+            navigator.clipboard.writeText(newElementHeader.innerHTML);
+        })
+    });
 });
 
-// console.log(resultArray)
